e3969f073e8d770560be6c04e0f7ee0330cbaff6
mickijames <mwilliams27@vcd-ceb131-03.eastern.ewu.edu>
2017-04-13 16:09:16 -0700

    Working Weather
    * [ ] Style and make look good

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,25 @@
-// Scripts
-console.log('Hello World!');
+// Get Weather
+$.simpleWeather({
+    location: 99004,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);
+      $('.image img').attr('src', weather.image);
+      $('.description p').text(weather.text);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
+  
+  });
+
+// Turn on Accordion
+$('.ui.accordion')
+  .accordion()
+;
